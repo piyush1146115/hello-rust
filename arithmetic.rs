45b85458b46228c921cf7c1b204f86431ea0c13b
piyush1146115 <piyush@appscode.com>
2022-11-14 17:57:02 +0600

    Explore rust arithmetic.

--- arithmetic.rs
+++ arithmetic.rs
@@ -1,10 +1,36 @@
 fn main(){
-  print!("{} + {} = {}\n", 34, 80, 80 + 34);
-  print!("{}\n", (23 - 6) % 5 + 20 * 30 / (3 + 4));
+      print!("{} + {} = {}\n", 34, 80, 80 + 34);
+      print!("{}\n", (23 - 6) % 5 + 20 * 30 / (3 + 4));
 
-//   By putting a dot after a literal number, it is transformed into a literal floating-point
-//   number. Some programming languages require a digit after the dot, but not Rust
-  print!("{}\n", (23. - 6.) % 5. + 20. * 30. / (3. + 4.));
-  print!("{} {}\n", -12 % 10, -1.2 % 1.);
-// This will print -2 -0.19999999999999996.
+      //   By putting a dot after a literal number, it is transformed into a literal floating-point
+      //   number. Some programming languages require a digit after the dot, but not Rust
+      print!("{}\n", (23. - 6.) % 5. + 20. * 30. / (3. + 4.));
+      print!("{} {}\n", -12 % 10, -1.2 % 1.);
+      // This will print -2 -0.19999999999999996.
+
+      // It will print 87.71428571428571.
+      print!("{}\n", (23. - 6.) % 5. + 20. * 30. / (3. + 4.));
+
+      // This is generate a compilation error. in Rust you cannot simply mix integer
+      // numbers and floating-point numbers.
+      // print!("{}", 2.7 + 1);
+
+      print!("{} + ", 80);
+      print!("{} =", 34);
+      print!(" {}\n", 80 + 34);
+
+
+      //Breaking Literal Strings
+      println!("{}\n", "These
+            are 
+            three lines");
+
+      println!("{}\n", "This \
+            is \
+            just one line");
+
+      println!("{}", "These\n\
+            are\n\
+            three lines");
+
 }
